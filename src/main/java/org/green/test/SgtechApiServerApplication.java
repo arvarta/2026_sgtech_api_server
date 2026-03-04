8329665d4502c075f4a9397ba863e53e43cd3c32
arvarta <arvarta@gmail.com>
2026-03-04 10:09:22 +0900

    2026 롯데 7회차 프로젝트1 1조(sgtech_api_server)

---
+++ src/main/java/org/green/test/SgtechApiServerApplication.java
@@ -0,0 +1,13 @@
+package org.green.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgtechApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgtechApiServerApplication.class, args);
+	}
+
+}
